a9df0e0b8af6e42b93b040f2362067bef0085034
Espen Norderud <espen42@gmail.com>
2019-02-24 16:56:40 +0100

    CLI support, README updates, input validation and unit testing out the wazoo.

--- README.md
+++ README.md
@@ -1,32 +1,78 @@
-# React4XP build constants
+# react4xp-buildconstants
 
-`index.js` exposes the method `getConstants(rootDir [, overrideJsonFilepath])`, which returns shared constants for buildtime component libraries for Enonic React4XP and its multiple build steps - mainly in **Webpack**. This defines the default expected source and build folder structure for **React4xp**, for the other librarified build steps to work.
+`index.js` exposes a default function `(rootDir, outputFile [, overrides])`, which builds an object that's stored in a standard JSON file. This object has shared constants mainly for building **React4XP** and component libraries. It is meant to define the source and target folder structure of Reaxt4XP.
 
-`getConstants` needs a `rootDir` argument (string), which is the root directory of the webpack project running it. Supplying `__dirname` should do the trick.
+## Install
 
-It returns an object with these attributes and default values (slashes depend on file system, should work correctly out of the box). 
+NPM or Yarn as usual:
+```bash
+npm add --save-dev react4xp-buildconstants
+```
+
+
+## Usage
+
+In Node context:
+```EcmaScript6
+const buildConstants = require('react4xp-buildconstants');
+buildConstants(rootDir, outputFile [, overrides])
+```
+
+Standalone, from command-line with Node installed:
+```bash
+node_modules/react4xp-buildconstants/lib/cli.js "rootDir" "outputFile" ["overrides"]
+```
+
+`rootDir` (string): the root directory of the React4XP project. Must exist.
+
+`outputFile` (string): the name of the JSON file where the constants are stored. If it doesn't end in `.json`, that extension will be added. It can / should be a full path-and-filename (in system-appropriate format). But if it's only a file name, the output file will be created in `rootDir`. Default behavior is to NOT overwrite the file if it already exists, but overwriting can be enabled (see `overwriteConstantsFile` below). Will try to create folder if path doesn't exist.
+
+`overrides` (optional. Object or JSON-parsable string): Override the default value(s) of output attributes by adding the same key and a new value here. This can be done for each __output value__ specifically (see the [output](#output) below), __and/or__ by setting the __base values__: some output values are derived from common base values. Although these base values will not be part of the output, you can override them here to control the several output values at once. Simpler, more consistent, and safer - override the base values if you can! The base attributes to override, and their current default values are:
+```json
+{
+  // Main source code folder, home of core (non-XP-specific) React4xp source code
+  R4X_HOME: "react4xp", 
+
+  // Absolute base source code folder, parent folder of not only R4X_HOME but also Enonic XP's `resources` and `java` folders. 
+  SRC_MAIN: "<rootDir>/src/main",
+
+  // The target runtime folder, into which React4xp components and runtime stuffs are transpiled
+  R4X_TARGETSUBDIR: "react4xp",                   
+  
+  // Base pre-JAR folder for building, relative to <rootDir>. Parent folder of R4X_TARGETSUBDIR
+  SUBFOLDER_BUILD_MAIN: "build/resources/main"
+
+  // ------ Modifying these won't change output, only adjust behavior when running:
+  verbose: false, // Some logging when true
+  overwriteConstantsFile: false, // Will overwrite already existing output JSON file if true
+}
+```
+
+Slashes depend on file system, should work correctly out of the box. Use your system's appropriate paths when overriding.
+
+
+## Output
+
+It builds a JSON file with these attributes. These default values can be controlled with the `overrides` parameter (but overriding base attributes instead of derived ones is probably simpler and safer):
 
   - `BUILD_ENV = "development"`: environment variable for production or development
   
-  - `SRC_MAIN = "<rootDir>/src/main"`: _full path_ to the folder below which both the main `react4xp` folder and Enonic XP's `resources` and `java` folders are found. 
-  
-  - `R4X_HOME = "react4xp"`: main react4xp source _subfolder_ - the name of the subfolder for the core (non-XP-specific) React4xp source code
-  - `SRC_R4X = "<SRC_MAIN>/<R4X_HOME>"`: _full path_ to the main react4xp source folder - the source folder for the core  React4XP sourcecode 
-  
-  - `SITE = "site"`: the name of the _subfolder_ below `<SRC_MAIN>/resources/` where the XP site structure is found
-  - `SRC_SITE = "<SRC_MAIN>/resources/<SITE>"`: _full path_ to the folder where the XP site structure is found
-  
-  - `R4X_ENTRY_SUBFOLDER = "_components"`: the name of the _subfolder_ inside the core react4xp folder where react4xp during buildtime will look for general (not bound to specific XP components) entry files (entries are non-shared, first-level components, runnable in both the client and in XP after transpilation)
-  - `SRC_R4X_ENTRIES = "<SRC_R4X>/<R4X_ENTRY_SUBFOLDER>"`: _full path_ to the entries source folder
-  
-  - `BUILD_MAIN = "<rootDir>/build/resources/main"`: _full path_ to the main target buildtime folder (pre-JAR)
-  
-  - `R4X_TARGETSUBDIR = "react4xp"`: name of target react4xp _subfolder_ into which both the React4xp core code and all (both shared and entry) React components will be built. 
-  - `BUILD_R4X = "<BUILD_MAIN>/<R4X_TARGETSUBDIR>"`: _full path_ to target folder into which both the React4xp core code and all (both shared and entry) React components will be built. 
-  - `RELATIVE_BUILD_R4X = "build/resources/main/<R4X_TARGETSUBDIR>"`: _relative path_ below the project folder, to the target react4xp build folder
-  
-  - `LIBRARY_NAME = "React4xp"`: The name of the runnable library, used for runtime calls in both the client and during serverside rendering 
-  
-  - `EXTERNALS = { "react": "React", "react-dom": "ReactDOM", "react-dom/server": "ReactDOMServer" }`: externals (non-React4xp, non-vendors) libraries needed to be runtime-available to both client and react serverside-rendering. Defined in `constants.json`, in the `EXTERNALS` field.  
+  - `LIBRARY_NAME = "React4xp"`: name of the runtime JS library, used for calls in both the client and during serverside rendering 
 
-The second `overrideJsonFilepath` argument (string) is the path and filename to a JSON file. This contains a single object. If attributes from this match the attributes above, the values will override the default values. Any other attributes will be added.
+  - `SITE_SUBFOLDER = "site"`: name of the __subfolder__ (below `<SRC_MAIN>/resources/`) where the Enonic XP site structure is found.
+
+  - `SRC_SITE = "<rootDir>/src/main/resources/site"`: __full path__ to the folder where the XP site structure is found. Derived from `SRC_MAIN + "resources" + SITE_SUBFOLDER`.
+  
+  - `R4X_ENTRY_SUBFOLDER = "_components"`: name of the __subfolder__ inside the core react4xp folder where react4xp during buildtime will look for general (not bound to specific XP components) __entry files__ (entries are non-shared, first-level components, runnable in both the client and in XP after transpilation)
+  
+  - `SRC_R4X = "<rootDir>/src/main/react4xp"`: _full path_ to the main react4xp source folder, home of core (non-XP-specific) React4xp source code. Derived from `SRC_MAIN + R4X_HOME`.
+  
+  - `SRC_R4X_ENTRIES = "<rootDir>/src/main/react4xp/_components"`: _full path_ to the entries source folder. Derived from `SRC_R4X + R4X_ENTRY_SUBFOLDER`.
+  
+  - `RELATIVE_BUILD_R4X = "build/resources/main/react4xp"`: _relative path_ to the target react4xp build folder. Derived from `SUBFOLDER_BUILD_MAIN + R4X_TARGETSUBDIR`.
+  
+  - `BUILD_MAIN = "<rootDir>/build/resources/main"`: _full path_ to the main target buildtime folder (pre-JAR). Derived from `SUBFOLDER_BUILD_MAIN`. 
+
+  - `BUILD_R4X = "<rootDir>/build/resources/main/react4xp"`: _full path_ to target folder into which both the React4xp core code and all (both shared and entry) React components will be built. Derived from `BUILD_MAIN + R4X_TARGETSUBDIR`
+  
+  - `EXTERNALS = { "react": "React", "react-dom": "ReactDOM", "react-dom/server": "ReactDOMServer" }`: externals (non-React4xp, non-vendors) libraries needed to be runtime-available by these names, to both client and react serverside-rendering.
--- test/buildconstants_spec.js
+++ test/buildconstants_spec.js
@@ -2,7 +2,7 @@ import path from 'path';
 import fs from 'fs';
 import rimraf from 'rimraf';
 import deepFreeze from 'deep-freeze';
-import { expect, fa } from 'chai';
+import { expect } from 'chai';
 
 import buildConstants from '../lib';
 const ensureTargetOutputPathExists = require('../src/ensureTargetOutputPathExists');
@@ -23,23 +23,32 @@ describe("constants", ()=>{
             SRC_R4X: path.join(DIR_NAME, 'src', 'main', 'react4xp'),
             SRC_R4X_ENTRIES: path.join(DIR_NAME, 'src', 'main', 'react4xp', '_components'),
             RELATIVE_BUILD_R4X: path.join('build', 'resources', 'main', 'react4xp'),
+            BUILD_MAIN: path.join(DIR_NAME, 'build', 'resources', 'main'),
             BUILD_R4X: path.join(DIR_NAME, 'build', 'resources', 'main', 'react4xp'),
             EXTERNALS: {
-              "react": "React",
-              "react-dom": "ReactDOM",
-              "react-dom/server": "ReactDOMServer"
-            }
+                "react": "React",
+                "react-dom": "ReactDOM",
+                "react-dom/server": "ReactDOMServer",
+            },
         });
 
         const TEST_OUTPUT_ROOT = path.join(DIR_NAME, "output");
+
+
+
+
 
         before(()=>{
             rimraf.sync(TEST_OUTPUT_ROOT);
         });
 
         after(()=>{
-            rimraf.sync(TEST_OUTPUT_ROOT);
+            //rimraf.sync(TEST_OUTPUT_ROOT);
         });
+
+
+
+
 
         it("builds a constants file with the specified output name, if the output file or path doesn't exist", () => {
             const outputFileName = path.join(TEST_OUTPUT_ROOT, "deep", "path", "built_constants.json");
@@ -50,8 +59,9 @@ describe("constants", ()=>{
                 //{verbose: true}
             );
             
-            const actualOutput = deepFreeze(require(outputFileName));
+            const actualOutput = require(outputFileName);
 
+            delete actualOutput['__META__'];
             expect(actualOutput).to.deep.equal(EXPECTED_DEFAULT_OUTPUT);
         });
 
@@ -93,8 +103,9 @@ describe("constants", ()=>{
                 }
             );
             
-            const actualOutput = deepFreeze(require(outputFileName));
+            const actualOutput = require(outputFileName);
 
+            delete actualOutput['__META__'];
             expect(actualOutput).to.deep.equal(EXPECTED_DEFAULT_OUTPUT);
         });
 
@@ -108,8 +119,10 @@ describe("constants", ()=>{
                 outputFileNameRaw, 
                 //{ verbose: true, }
             );
-            
-            const actualOutput = deepFreeze(require(outputFileNameJSON));
+
+            const actualOutput = require(outputFileNameJSON);
+
+            delete actualOutput['__META__'];
             expect(actualOutput).to.deep.equal(EXPECTED_DEFAULT_OUTPUT);
         });
 
@@ -118,11 +131,15 @@ describe("constants", ()=>{
             const outputFileName = "purefilename_constants.json";
 
             buildConstants(
-                TEST_OUTPUT_ROOT,       // Changing the rootDir arg to avoid putting the output file right here. That will affect the file content of course, different expectations below.
+
+                // Changing the rootDir arg to avoid putting the output file right here.
+                // That will affect the file content of course, different expectations below.
+                TEST_OUTPUT_ROOT,
+
                 outputFileName, 
                 //{ verbose: true, }
             );
-            
+
             const actualOutput = deepFreeze(require(path.join(TEST_OUTPUT_ROOT, outputFileName)));
             
             // Just sampling a few unchanged ones
@@ -147,7 +164,7 @@ describe("constants", ()=>{
                 {
                     R4X_ENTRY_SUBFOLDER: "thisWasPreviously_components",
                     SITE_SUBFOLDER: "thisWasPreviouslySite",
-                    //verbose: true
+                    //verbose: true,
                 }
             );
             
@@ -160,8 +177,9 @@ describe("constants", ()=>{
             // The two directly changed ones
             expect(actualOutput.R4X_ENTRY_SUBFOLDER).to.equal("thisWasPreviously_components");
             expect(actualOutput.SITE_SUBFOLDER).to.equal("thisWasPreviouslySite");
-            
         });
+
+
 
 
         it("changes derived values from overriden ones", () => {
@@ -185,10 +203,12 @@ describe("constants", ()=>{
             expect(actualOutput.RELATIVE_BUILD_R4X).to.equal(EXPECTED_DEFAULT_OUTPUT.RELATIVE_BUILD_R4X);
 
             // All the derived changes:
-            expect(actualOutput.SRC_R4X).to.deep.equal(path.join(DIR_NAME, 'src', 'main', 'thisWasPreviouslyReact4xp'));
+            expect(actualOutput.SRC_R4X).to.equal(path.join(DIR_NAME, 'src', 'main', 'thisWasPreviouslyReact4xp'));
             expect(actualOutput.SRC_R4X_ENTRIES).to.equal(path.join(DIR_NAME, 'src', 'main', 'thisWasPreviouslyReact4xp', '_components'),);
             expect(actualOutput.SRC_SITE).to.equal(path.join(DIR_NAME, 'src', 'main', 'resources', 'thisWasPreviouslySite'));
         });
+
+
 
 
         it("can override specific derived values without changing the basic ones", () => {
@@ -209,13 +229,211 @@ describe("constants", ()=>{
             expect(actualOutput.LIBRARY_NAME).to.equal(EXPECTED_DEFAULT_OUTPUT.LIBRARY_NAME);
             expect(actualOutput.EXTERNALS).to.deep.equal(EXPECTED_DEFAULT_OUTPUT.EXTERNALS);
             expect(actualOutput.RELATIVE_BUILD_R4X).to.equal(EXPECTED_DEFAULT_OUTPUT.RELATIVE_BUILD_R4X);
-            expect(actualOutput.SRC_R4X).to.deep.equal(EXPECTED_DEFAULT_OUTPUT.SRC_R4X);
+            expect(actualOutput.SRC_R4X).to.equal(EXPECTED_DEFAULT_OUTPUT.SRC_R4X);
             expect(actualOutput.SRC_R4X_ENTRIES).to.equal(EXPECTED_DEFAULT_OUTPUT.SRC_R4X_ENTRIES);
             // ...and notably:
             expect(actualOutput.SITE_SUBFOLDER).to.equal(EXPECTED_DEFAULT_OUTPUT.SITE_SUBFOLDER);
             
             // All the derived changes:
             expect(actualOutput.SRC_SITE).to.equal("thisShouldBeAnEntireOverwrittenSrcSitePath"); 
+        });
+
+
+
+
+        it("can also accept the 'overrides' parameter as a JSON-parseable string", ()=>{
+            const outputFileName = path.join(TEST_OUTPUT_ROOT, "deep", "path", "override_jsonstring_constants.json");
+
+            const overrides = '{' +
+                '"R4X_ENTRY_SUBFOLDER": "thisWasPreviously_components", ' +
+                '"SITE_SUBFOLDER": "thisWasPreviouslySite", ' +
+                '"EXTERNALS": {' +
+                '"foo": "foofoo",' +
+                '"bar": "barbar"' +
+                '}' +
+                // ', "verbose": true ' +
+                '}';
+
+            buildConstants(
+                DIR_NAME,
+                outputFileName,
+                overrides,
+            );
+
+            const actualOutput = deepFreeze(require(outputFileName));
+
+            // Just sampling a few unchanged ones
+            expect(actualOutput.LIBRARY_NAME).to.equal(EXPECTED_DEFAULT_OUTPUT.LIBRARY_NAME);
+            expect(actualOutput.BUILD_ENV).to.deep.equal(EXPECTED_DEFAULT_OUTPUT.BUILD_ENV);
+
+            // The two directly changed ones
+            expect(actualOutput.R4X_ENTRY_SUBFOLDER).to.equal("thisWasPreviously_components");
+            expect(actualOutput.SITE_SUBFOLDER).to.equal("thisWasPreviouslySite");
+            expect(actualOutput.EXTERNALS).to.deep.equal({
+                foo: "foofoo",
+                bar: "barbar",
+            });
+        });
+
+
+
+
+        it("fails if overrides is an invalid JSON string", ()=>{
+            const outputFileName = path.join(TEST_OUTPUT_ROOT, "deep", "path", "bad_jsonstring_constants.json");
+
+            const overrides = '{' +
+                '"R4X_ENTRY_SUBFOLDER": "thisWasPreviously_components", ' +
+                '"SITE_SUBFOLDER: "thisWasPreviouslySite", ' +
+                // ', "verbose": true ' +
+                '}';
+
+            expect(()=>{
+                buildConstants(
+                    DIR_NAME,
+                    outputFileName,
+                    overrides,
+                );
+            }).to.throw(Error);
+
+
+            expect(fs.existsSync(outputFileName)).to.equal(false);
+        });
+
+
+        it("fails if overrides (JSON string or object) is valid but not object format", ()=>{
+            const outputFileName = path.join(TEST_OUTPUT_ROOT, "deep", "path", "bad_jsonstring_constants.json");
+
+            expect(()=>{
+                buildConstants(
+                    DIR_NAME,
+                    outputFileName,
+                    '[ "R4X_ENTRY_SUBFOLDER", "SITE_SUBFOLDER" ]',
+                );
+            }).to.throw(Error);
+
+            expect(()=>{
+                buildConstants(
+                    DIR_NAME,
+                    outputFileName,
+                    [ "R4X_ENTRY_SUBFOLDER", "SITE_SUBFOLDER" ],
+                );
+            }).to.throw(Error);
+
+            expect(()=>{
+                buildConstants(
+                    DIR_NAME,
+                    outputFileName,
+                    3,
+                );
+            }).to.throw(Error);
+
+            expect(()=>{
+                buildConstants(
+                    DIR_NAME,
+                    outputFileName,
+                    true,
+                );
+            }).to.throw(Error);
+
+            expect(fs.existsSync(outputFileName)).to.equal(false);
+        });
+
+
+        it("fails if rootDir points to a path that doesn't exist", ()=>{
+            const outputFileName = path.join(TEST_OUTPUT_ROOT, "deep", "path", "noexist_constants.json");
+
+            const noExistPath = path.join(TEST_OUTPUT_ROOT, "noExist");
+            rimraf.sync(noExistPath);
+
+            expect(()=>{
+                buildConstants(
+                    noExistPath,
+                    outputFileName,
+                    //{verbose:true},
+                );
+            }).to.throw(Error);
+
+            expect(fs.existsSync(outputFileName)).to.equal(false);
+
+            expect(()=>{
+                buildConstants(
+                    undefined,
+                    outputFileName,
+                    //{verbose:true},
+                );
+            }).to.throw(Error);
+        });
+
+
+        it("fails if rootDir points to a file", ()=>{
+            const outputFileName = path.join(TEST_OUTPUT_ROOT, "deep", "path", "isfile_constants.json");
+
+            const fileExistPath = path.join(TEST_OUTPUT_ROOT, "thisIsAFile");
+            fs.writeFileSync(fileExistPath, "Yep, a file all right.");
+
+            expect(()=>{
+                buildConstants(
+                    fileExistPath,
+                    outputFileName,
+                    //{verbose:true},
+                );
+            }).to.throw(Error);
+
+            expect(fs.existsSync(outputFileName)).to.equal(false);
+        });
+
+
+        it("fails if outputFile name parameter is empty or missing", ()=>{
+            expect(()=>{
+                buildConstants(
+                    DIR_NAME,
+                    "",
+                    //{verbose:true},
+                );
+            }).to.throw(Error);
+
+            expect(()=>{
+                buildConstants(DIR_NAME);
+            }).to.throw(Error);
+        });
+
+
+
+
+        it("fails if outputFile points to a directory", ()=>{
+            const outputFileName = path.join(TEST_OUTPUT_ROOT, "thisisDirectory_constants.json");
+
+            ensureTargetOutputPathExists(path.join(outputFileName, "dummy"), function () {});
+            expect(fs.lstatSync(outputFileName).isDirectory()).to.equal(true);
+
+            expect(()=>{
+                buildConstants(
+                    DIR_NAME,
+                    outputFileName,
+                    //{verbose:true},
+                );
+            }).to.throw(Error);
+
+            expect(fs.lstatSync(outputFileName).isDirectory()).to.equal(true);
+        });
+
+
+
+        it("fails if outputFile has a parent path that's not directory", ()=>{
+            const outputFileName = path.join(TEST_OUTPUT_ROOT, "thisIsAlsoAFile", "parentisfile_constants.json");
+
+            const fileExistPath = path.join(TEST_OUTPUT_ROOT, "thisIsAlsoAFile");
+            fs.writeFileSync(fileExistPath, "Indeed, 'tis a file.");
+
+            expect(()=>{
+                buildConstants(
+                    DIR_NAME,
+                    outputFileName,
+                    //{verbose:true},  t
+                );
+            }).to.throw(Error);
+
+            expect(fs.existsSync(outputFileName)).to.equal(false);
         });
     });
 });
